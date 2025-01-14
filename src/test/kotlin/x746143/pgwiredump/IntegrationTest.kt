7dac4c7da3edae6d31c5609941c313926439cdbf
0x746143 <176037012+0x746143@users.noreply.github.com>
2025-01-14 14:05:50 +0100

    Create integration tests

---
+++ src/test/kotlin/x746143/pgwiredump/IntegrationTest.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2025 0x746143
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package x746143.pgwiredump
+
+import java.io.ByteArrayOutputStream
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class IntegrationTest : PgContainerLifecycle() {
+
+    @Test
+    fun testSimpleQuery() {
+        val output = ByteArrayOutputStream()
+        container.jdbcClient.connect().use {
+            container.dumpPgTraffic(output) {
+                val count = it.simpleQuery("select * from basic_types_table")
+                assertEquals(2, count)
+            }
+        }
+        verifyContent("basic_types_table.txt", output)
+    }
+
+    @Test
+    fun testTcpFragmentedDataParsing() {
+        val output = ByteArrayOutputStream()
+        container.jdbcClient.connect().use {
+            container.dumpPgTraffic(output) {
+                val count = it.simpleQuery("select * from pg_catalog.pg_type")
+                assertEquals(619, count)
+            }
+        }
+        verifyContent("pg_catalog.pg_type.txt", output)
+    }
+}
---
+++ src/test/kotlin/x746143/pgwiredump/PgContainerLifecycle.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2025 0x746143
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package x746143.pgwiredump
+
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.TestInstance
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+open class PgContainerLifecycle {
+    protected lateinit var container: PgTsharkContainer
+
+    @BeforeAll
+    fun setUp() {
+        container = PgTsharkContainer()
+        container.start()
+    }
+
+    @AfterAll
+    fun tearDown() {
+        container.stop()
+    }
+}
